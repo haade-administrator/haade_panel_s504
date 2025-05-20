b38f2cf3318b8c8e3553ce83311f36a08ffddd13
Administrateur <administrateur@haade.fr>
2025-05-20 22:15:02 +0200

    add bootloader

---
+++ android/app/src/main/kotlin/com/example/mqtt_hatab/BootReceiver.java
@@ -0,0 +1,18 @@
+package com.example.mqtt_hatab;  // même package que MainActivity
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Log.d("BootReceiver", "Démarrage détecté !");
+            Intent launchIntent = new Intent(context, MainActivity.class);
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(launchIntent);
+        }
+    }
+}
