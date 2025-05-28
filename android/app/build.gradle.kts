c78c01e4274f1fadeb815b0f7d94bd2b427a497b
Administrateur <administrateur@haade.fr>
2025-05-28 19:56:15 +0200

    add key release property

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,25 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+} else {
+    throw GradleException("Le fichier key.properties est introuvable à la racine du projet.")
 }
 
 android {
     namespace = "com.example.haade_panel_s504"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -19,10 +30,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.haade_panel_s504"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -30,7 +38,6 @@ android {
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a")
         }
-
         externalNativeBuild {
             cmake {
                 cppFlags += ""
@@ -38,18 +45,35 @@ android {
         }
     }
 
+    signingConfigs {
+    create("release") {
+        keyAlias = keystoreProperties["keyAlias"]?.toString()
+        keyPassword = keystoreProperties["keyPassword"]?.toString()
+        storePassword = keystoreProperties["storePassword"]?.toString()
+
+        val storeFilePath = keystoreProperties["storeFile"]?.toString()
+        if (storeFilePath != null) {
+            storeFile = file(storeFilePath)
+        } else {
+            throw GradleException("storeFile manquant dans key.properties")
+        }
+      }
+    }   
+
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
+
     sourceSets {
         getByName("main") {
             jniLibs.srcDirs("src/main/jniLibs")
         }
     }
+
     externalNativeBuild {
         cmake {
             path = file("CMakeLists.txt")
